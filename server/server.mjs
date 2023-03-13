cd6977209ac7f3044609e8cf0b5eaea505032268
tranhieu <tranhieu1892001@gmail.com>
2023-03-13 13:12:19 +0700

    update /graphql for socket

--- server/server.mjs
+++ server/server.mjs
@@ -44,7 +44,7 @@ const wsServer = new WebSocketServer({
   server: httpServer,
   // Pass a different path here if app.use
   // serves expressMiddleware at a different path
-  path: '/',
+  path: '/graphql',
 })
 
 // Hand in the schema we just created and have the
